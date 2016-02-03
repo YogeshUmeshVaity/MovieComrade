b36f11ded79cc744cbf922eb1f7c57518467a4d3
Yogesh <yogeshvaity@gmail.com>
2016-02-03 18:16:53 +0530

    Add layout for single-grid

--- app/src/main/java/com/compmaestros/moviecomrade/MainFragment.java
+++ app/src/main/java/com/compmaestros/moviecomrade/MainFragment.java
@@ -21,6 +21,7 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
