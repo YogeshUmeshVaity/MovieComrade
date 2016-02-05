612e0ff4f6a7c98729816e246f2a6fe02a0aa550
Yogesh <yogeshvaity@gmail.com>
2016-02-05 18:00:48 +0530

    Fix fragment overlapping bug.

--- app/src/main/java/com/compmaestros/moviecomrade/MainActivity.java
+++ app/src/main/java/com/compmaestros/moviecomrade/MainActivity.java
@@ -9,7 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.main_fragment_container, new MainFragment()).commit();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.main_fragment_container, new MainFragment()).commit();
+        }
     }
 }
